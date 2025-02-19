d271ec10ab77f2b7950522eb509db9a9a2f63c4e
Raiyan109 <kraiyan109@gmail.com>
2025-02-19 16:14:20 +0600

    loading with isLoading and ActivityIndicator

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,29 +1,43 @@
-import { View, Text, FlatList, SafeAreaView, StyleSheet } from 'react-native'
+import { View, Text, FlatList, SafeAreaView, StyleSheet, ActivityIndicator } from 'react-native'
 import React, { useEffect, useState } from 'react'
 
-type IProps = {
+type IUser = {
   name: string;
   email: string
 }
 
 const App = () => {
   const [users, setUsers] = useState([])
+  const [isLoading, setIsLoading] = useState(false)
+
+
   const fetchData = async () => {
+    setIsLoading(true)
     const response = await fetch(`https://wesoftin-backend.vercel.app/users`)
     const data = await response.json()
 
     setUsers(data)
+    setIsLoading(false)
   }
 
   useEffect(() => {
     fetchData()
   }, [])
+
+  if (isLoading) {
+    return (
+      <SafeAreaView style={styles.loadingContainer}>
+        <ActivityIndicator size='large' color={'#370617'} />
+        <Text> Loading...</Text>
+      </SafeAreaView>
+    )
+  }
   return (
     <SafeAreaView style={styles.container}>
       <View style={styles.listContainer}>
         <FlatList
           data={users}
-          renderItem={({ item }: { item: IProps }) => {
+          renderItem={({ item }: { item: IUser }) => {
             return (
               <View style={styles.card}>
                 <Text>Name: {item.name}</Text>
@@ -56,5 +70,12 @@ const styles = StyleSheet.create({
   },
   listContainer: {
     display: 'flex'
+  },
+  loadingContainer: {
+    flex: 1,
+    backgroundColor: '#f4f1de',
+    paddingTop: 3,
+    justifyContent: 'center',
+    alignItems: 'center'
   }
 })
